dc7dca6758a276b422c9ae49ce57883b1ef883ec
John Li <john166@rocketmail.com>
2018-12-26 16:19:24 -0800

    Why is javascript like this

--- viewerfrontend.js
+++ viewerfrontend.js
@@ -1,12 +1,72 @@
 
-let modeArr = [];
-let board = document.getElementById("board");
+const BOARD = document.getElementById("board");
+const ONE_SECOND = 1000;
+let viewers = [];
+//want to sort by values but rearrange viewer ordering. most likeley will
+//sort into an array and hash for each time to display onto board.
+let searchable = new Map();
+
+window.Twitch.ext.onAuthorized(function(ids){
+    searchable.set(ids.userId, new TimeTracker(ids.userId));
+    console.log("onAuthorized fired");
+});
 
 window.Twitch.ext.onContext(function(cxt, changeArr){
-    modeArr.push(cxt.mode);
+    console.log("onContext fired");
 });
 
 function display(){
-    let newTime = document.createTextNode(modeArr);
-    board.appendChild(newTime);
+    //TODO change to viewers later, map will remain unsorted for O(1) search.
+    for(let [keys, values] of searchable.entries()){
+        let newItem = document.createElement("li");
+        let newTime = document.createTextNode(keys + " " + values);
+        BOARD.appendChild(newItem);
+    }
 }
+
+function refresh(){
+
+}
+
+//will add actual documentation later
+class TimeTracker{
+
+
+    constructor(user){
+        this.user = user;
+        this.time = 0;
+        this.paused = false;
+
+        this.tracker = this.startTime();
+    }
+
+    startTime(){
+
+        let currTimer = this;
+
+        let tracker = setInterval(function(){
+            if(!this.paused){
+                currTimer.time += 1;
+            }
+        }, ONE_SECOND);
+        
+        return tracker;
+
+    }
+
+    //gets called when user stops viewing.
+    stopTime(){
+        clearInterval(this.tracker);
+    }
+
+    //gets called when user pauses stream.
+    pauseTime(){
+        this.paused = true;
+    }
+
+    unpauseTime(){
+        this.paused = false;
+    }
+
+}
+
