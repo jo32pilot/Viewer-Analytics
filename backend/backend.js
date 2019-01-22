a6818a38ebdf51db0d5663569be51e1a22bf7340
John Li <john166@rocketmail.com>
2019-01-21 22:40:40 -0800

    Set up skeleton of web server

--- backend/backend.js
+++ backend/backend.js
@@ -2,14 +2,60 @@
  * @fileoverview Backend for Viewer Analytics Twitch extension. Handles API
  * calls, database queries, viewer queries, and broadcaster configurations.
  * Dependencies:
+ *      - jsdom
  *      - jQuery
- *      - [New Twitch API](https://dev.twitch.tv/docs/api/)
  *      - [jsrsasign](https://github.com/kjur/jsrsasign)
  */
 
-let jwt = require("jsrsasign");
+const jwt = require("jsrsasign");
+const https = require("https");
+const fs = require("fs");
+
+//Importing jQuery library. Credit for this work around goes to 
+//https://github.com/rstacruz/mocha-jsdom/issues/27
+const jsdom = require("jsdom");
+const {JSDOM} = jsdom;
+const {window} = new JSDOM();
+const {document} = (new JSDOM('')).window;
+global.document = document;
+const $ = require("jquery")(window);
 
 //All other constants go in config.json
-const JSON_PATH = "config.json";
-let json = $.getJSON(JSON_PATH);
+const JSON_PATH = "./config.json";
+const json = require(JSON_PATH);
 
+const options = {
+
+    cert: fs.readFileSync(json.certPath),
+    key: fs.readFileSync(json.keyPath),
+
+};
+
+const server = https.createServer(options, function(req, res){
+
+    const headers = json.headers;
+
+    if(req.method == "OPTIONS"){
+
+        res.writeHead(200, headers);
+        res.end();
+
+    }
+    
+    if(req.method == "GET" && req.url == json.initBoard){
+
+        res.writeHead(200, headers);
+        res.end(getData());
+
+    }
+
+}).listen(json.port);
+
+
+/**
+ * Retrieves data from database.
+ */
+function getData(){
+    //temporary
+    return "{\"data\":\"sample\", \"data2\":\"sample2\"}";
+}
--- frontend/viewerfrontend.js
+++ frontend/viewerfrontend.js
@@ -6,43 +6,83 @@
 
 //---------- CONSTANTS ----------//
 
-const BACKEND_PATH = "";
+const SERVER_DOMAIN = "https://localhost:48091/";
+const AUTH_TITLE = "extension-jwt";
+const INITIAL_BOARD = "initBoard"
 
 //---------- SETTUP ----------//
 
-const board = document.getElementById("board");
+//maybe keep multiple arrays sorted different ways so we don't have to
+//sort every time?
 const viewers = [];
 
-//Object for average O(1) search for user times 
-//(Apparently they're implemented as hash maps)
-const searchable = {};
+// Object for average O(1) search for user times 
+// (Apparently they're implemented as hash maps)
+// Do not sort this into itself to put on the 
+// board. The whole point is to keep search O(1).
+var searchable = {};
 
 
 //---------- FUNCTIONS / EVENT LISTENERS ----------//
 
 window.Twitch.ext.onAuthorized(function(auth){
     $.ajax({
-        url: BACKEND_PATH,
+        url: SERVER_DOMAIN + INITIAL_BOARD,
         type: "GET",
         headers:{
-            "extension-jwt": auth.token,
-        }
+            AUTH_TITLE: auth.token,
+        },
+        success: initBoard
+        //TODO Define error handler
     });
+
+    //TODO Get rid of this line
     console.log("onAuthorized fired");
+
 });
 
 
 window.Twitch.ext.onContext(function(cxt, changeArr){
+    //TODO Get rid of this line
     console.log("onContext fired");
 });
 
+$(function(){
+
+    $("#refresh").on("click"), refresh);
+
+});
+
+
+/**
+ * Populates board and global variables for the first time.
+ *
+ */
+function initBoard(userTimes){
+
+    searchable = JSON.parse(userTimes);
+
+    //TODO sort searchable users into viewers array by time
+    
+    for (let user in searchable){
+        viewers.push([user, searchable[user]]);
+    }
+
+    viewers.sort(function(a, b){
+        return a[1] - b[1];
+    });
+
+    for (let user of viewers){
+        let item = $("<li>").text(`${user[0]}: ${user[1]}`);
+        $("#board").append(item);
+    }
+}
 
 /**
  * Displays leaderboard onto the iframe
  */
 function displayLeaderboard(){
 
-    }
 }
 
 
